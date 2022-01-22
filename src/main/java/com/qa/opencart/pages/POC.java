7eb8ef327812684a3e6b719a215a8b4d58d65ab9
Rudrika98 <64193635+Rudrika98@users.noreply.github.com>
2022-01-22 20:39:12 +0530

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class POC {
+
+	public static void main(String[] args) {
+		int i = 10; 
+		int h = 20; 
+		int sum = i + h; 
+		
+    String name = "Rudrika" ; 
+    String id = "12345";
+
+	}
+
+}
